1c783e24f90140236c3686a11e37cbddac0447ba
yan-xinyi <140530497+yan-xinyi@users.noreply.github.com>
2023-07-30 14:10:58 +0800

    Update ATT_BILSTMCRF.py

--- models/ATT_BILSTMCRF.py
+++ models/ATT_BILSTMCRF.py
@@ -1,77 +1,35 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2022/3/8 17:02
-# @Author  : leizhao150
-import numpy as np
+# @Time    : 2023/7/30 11:25
+# @Author  : Xinyi Yan
 
+'''
+   This python file mainly contains code for training, testing and evaluation of AKE models for soft attention based Bi-LSTM+CRF.
+   The main function is att_blcrf(), called from main().
+      1* Through the TextDataSet (Dataset): build the dictionary for vovabulary and cognitive signals.
+      2* Build ATT_BiLSTM_CRF() model
+      3* Start training and calculate the loss value.
+      4* Conduct testing, again read the data first, build the model, in the prediction, evaluate its results.
+'''
+
+import numpy as np
 import logging
 from torch import nn, optim
 from torch.nn import init
 import torch.nn.functional as F
 from tqdm import tqdm
-from config import *
-from utils import *
+from ..config import *
+from ..utils import *
+from ..evaluate import *
 from torchcrf import CRF
-from evaluate import *
 from torch.utils.data import Dataset, DataLoader
 
-torch.cuda.set_device(0)
 
-class TextDataSet(Dataset):
-
-    def __init__(self, data_path):
-        # Read the dictionary
-        self.word2ids = {word: i for i, word in enumerate(json.load(open(vocab_path, 'r', encoding='utf-8')))}
-        self.id2words = {val: key for key, val in self.word2ids.items()}
-
-        # Read the data
-        self.datas = list(json.load(open(data_path, 'r', encoding='utf-8')).values())
-
-    def __getitem__(self, item):
-        text = self.datas[item]
-        word_to_ids, tag_to_ids = [], []
-        eeg_list, et_list = [], []
-        i, text_len = 0, len(text)
-        attention_mask = []
-        while i < max_length:
-            if i < text_len:
-                word = text[i]
-                word_id = self.word2ids['[UNK]']
-                if word[0] in self.word2ids.keys():
-                    word_id = self.word2ids[word[0]]
-                tag_id = tag2ids[word[-1]]
-                et = list(map(float, word[1: 18]))
-                eeg = list(map(float, word[18: -1]))
-
-                word_to_ids.append(word_id)
-                tag_to_ids.append(tag_id)
-                et_list.append(et)
-                eeg_list.append(eeg)
-                attention_mask.append(1)
-            else:
-                word_to_ids.append(self.word2ids['[PAD]'])
-                tag_to_ids.append(tag2ids['[PAD]'])
-                et_list.append([0.0] * 17)
-                eeg_list.append([0.0] * 8)
-                attention_mask.append(0)
-            i += 1
-        return {
-            "input_ids": torch.tensor(word_to_ids).long().to(device),
-            "tags": torch.tensor(tag_to_ids).long().to(device),
-            "et": torch.tensor(et_list).float().to(device),
-            "eeg": torch.tensor(eeg_list).float().to(device),
-            "attention_mask": torch.tensor(attention_mask).byte().to(device),
-            "text_len": text_len
-        }
-
-    def __len__(self):
-        return len(self.datas)
-
-
-class att_BiLSTM_CRF(nn.Module):
+# Define ATT_BiLSTM_CRF() model
+class ATT_BiLSTM_CRF(nn.Module):
 
     def __init__(self, vocab_size, embed_dim,
                  hidden_dim, num_layers=1, num_tags=6):
-        super(att_BiLSTM_CRF, self).__init__()
+        super(ATT_BiLSTM_CRF, self).__init__()
 
         self.embedding = nn.Embedding(vocab_size, embed_dim, padding_idx=0)
         self.bilstm = nn.LSTM(input_size=embed_dim + fs_num,
@@ -139,22 +97,25 @@ class att_BiLSTM_CRF(nn.Module):
             return crf_feats, outputs
 
 
+# Main Function
 def att_blcrf():
 
-    # load the data
-    trainLoader = DataLoader(TextDataSet(train_path), batch_size=batch_size)
-    testLoader  = DataLoader(TextDataSet(test_path), batch_size=batch_size)
+    # Load the data
+    trainLoader = DataLoader(dataset=TextDataSet(train_path, vocab_path, max_length, tag2ids), batch_size=batch_size)
+    testLoader = DataLoader(TextDataSet(test_path, vocab_path, max_length, tag2ids), batch_size=batch_size)
 
-    model = att_BiLSTM_CRF(vocab_size= vocab_size,
+    # Deefine the model
+    model = ATT_BiLSTM_CRF(vocab_size= vocab_size,
                            embed_dim = embed_dim,
                            hidden_dim= hidden_dim).to(device)
-    print(model)
+    # Deefine the optimizer
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-    # Train the model
     best_P, best_R, best_F, best_epoch = 0.0, 0.0, 0.0, 0
     for epoch in range(epochs):
         print("epoch " + str(epoch + 1) + " is starting!")
+        
+        # Train the model
         model.train()
         avg_loss = []
         with tqdm(trainLoader) as pbar_train:
