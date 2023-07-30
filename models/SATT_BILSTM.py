acd46eed5a2da33a83a48666f09da4b7c93670d6
yan-xinyi <140530497+yan-xinyi@users.noreply.github.com>
2023-07-30 14:17:37 +0800

    Update SATT_BILSTM.py

--- models/SATT_BILSTM.py
+++ models/SATT_BILSTM.py
@@ -1,68 +1,27 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/1/1 17:00
+# @Time    : 2023/7/30 11:25
 # @Author  : Xinyi Yan
+
+'''
+   This python file mainly contains code for training, testing and evaluation of AKE models for self-attention based Bi-LSTM.
+   The main function is satt_bl(), called from main().
+      1* Through the TextDataSet (Dataset): build the dictionary for vovabulary and cognitive signals.
+      2* Build SATTBiLSTM() model
+      3* Start training and calculate the loss value.
+      4* Conduct testing, again read the data first, build the model, in the prediction, evaluate its results.
+'''
 
 import numpy as np
 import logging
 from torch import nn, optim
 from tqdm import tqdm
-from utils import *
-from config import *
-from evaluate import *
+from ..utils import *
+from ..config import *
+from ..evaluate import *
 from torch.utils.data import Dataset, DataLoader
 
 
-class TextDataSet(Dataset):
-
-    def __init__(self, data_path):
-        # read the dictionary
-        self.word2ids = {word: i for i, word in enumerate(json.load(open(vocab_path, 'r', encoding='utf-8')))}
-        self.id2words = {val: key for key, val in self.word2ids.items()}
-
-        # read the data
-        self.datas = list(json.load(open(data_path, 'r', encoding='utf-8')).values())
-
-    def __getitem__(self, item):
-        text = self.datas[item]
-        word_to_ids, tag_to_ids = [], []
-        eeg_list, et_list = [], []
-        i, text_len = 0, len(text)
-        attention_mask = []
-        while i < max_length:
-            if i < text_len:
-                word = text[i]
-                word_id = self.word2ids['[UNK]']
-                if word[0] in self.word2ids.keys():
-                    word_id = self.word2ids[word[0]]
-                tag_id = tag2ids[word[-1]]
-                et = list(map(float, word[1: 18]))
-                eeg = list(map(float, word[18: -1]))
-
-                word_to_ids.append(word_id)
-                tag_to_ids.append(tag_id)
-                et_list.append(et)
-                eeg_list.append(eeg)
-                attention_mask.append(1)
-            else:
-                word_to_ids.append(self.word2ids['[PAD]'])
-                tag_to_ids.append(tag2ids['[PAD]'])
-                et_list.append([0.0] * 17)
-                eeg_list.append([0.0] * 8)
-                attention_mask.append(0)
-            i += 1
-        return {
-            "input_ids": torch.tensor(word_to_ids).long().to(device),
-            "tags": torch.tensor(tag_to_ids).long().to(device),
-            "et": torch.tensor(et_list).float().to(device),
-            "eeg": torch.tensor(eeg_list).float().to(device),
-            "attention_mask": torch.tensor(attention_mask).byte().to(device),
-            "text_len": text_len
-        }
-
-    def __len__(self):
-        return len(self.datas)
-    
-
+# LayerNorm Function for the self-attention layer
 class LayerNorm(nn.Module):
     # Transform the q,k,v vector with dimension [batch_size * seq_length * hidden_size] into [batch_size * num_attention_heads * seq_length * attention_head_size]，便于后面做 Multi-Head Attention。
     def __init__(self, hidden_size, eps=1e-12):
@@ -80,6 +39,7 @@ class LayerNorm(nn.Module):
         return self.weight * x + self.bias
 
 
+# Define Self-Attention Mechanism Function
 class SelfAttention(nn.Module):
     def __init__(self, num_attention_heads, input_size, hidden_size, hidden_dropout_prob):
         super(SelfAttention, self).__init__()
@@ -150,6 +110,7 @@ class SelfAttention(nn.Module):
         return hidden_states
     
 
+# Define SATTBiLSTM() model
 class SATTBiLSTM(nn.Module):
 
     def __init__(self, vocab_size, embed_dim,hidden_dim,
@@ -183,21 +144,25 @@ class SATTBiLSTM(nn.Module):
         return output
 
 
+# Main Function
 def satt_bl():
 
-    trainLoader = DataLoader(dataset=TextDataSet(train_path), batch_size=batch_size)
-    testLoader = DataLoader(TextDataSet(test_path), batch_size=batch_size)
+    # Load the data
+    trainLoader = DataLoader(dataset=TextDataSet(train_path, vocab_path, max_length, tag2ids), batch_size=batch_size)
+    testLoader = DataLoader(TextDataSet(test_path, vocab_path, max_length, tag2ids), batch_size=batch_size)
 
+    # Deefine the model
     model = SATTBiLSTM(vocab_size=vocab_size,
                        embed_dim=embed_dim,
                        hidden_dim=hidden_dim).to(device)
-    print(model)
+    # Deefine the optimizer
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-    # Train the model
     best_P, best_R, best_F, best_epoch = 0.0, 0.0, 0.0, 0
     for epoch in range(epochs):
         print("epoch " + str(epoch + 1) + " is starting!")
+        
+        # Train the model
         model.train()
         avg_loss = []
         with tqdm(trainLoader) as pbar_train:
@@ -251,7 +216,5 @@ def satt_bl():
         logging.info("P is :" + P_str)
         logging.info("R is :" + R_str)
         logging.info("F is :" + F_str)
-
-    logging.info("best epoch is :" + str(best_epoch))
 
     return best_P, best_R, best_F, best_epoch
