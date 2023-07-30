6df5c7603fa3e399dd0112d6e3006e92d3d97e3f
yan-xinyi <140530497+yan-xinyi@users.noreply.github.com>
2023-07-30 14:32:22 +0800

    Update SATT_BILSTMCRF_GloVe.py

--- models/SATT_BILSTMCRF_GloVe.py
+++ models/SATT_BILSTMCRF_GloVe.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2022/3/8 17:02
-# @Author  : leizhao150
+# @Time    : 2023/7/30 11:25
+# @Author  : Xinyi Yan
+
+'''
+   This python file mainly contains code for training, testing and evaluation of Pre-trained model based AKE models for self-attention based Bi-LSTM+CRF adding GloVe Embeddings.
+   The main function is satt_blcrf_glove(), called from main().
+      1* Through the TextDataSet (Dataset): build the dictionary for vovabulary and cognitive signals.
+      2* Build SATT_BiLSTM_CRF_GL() model
+      3* Start training and calculate the loss value.
+      4* Conduct testing, again read the data first, build the model, in the prediction, evaluate its results.
+'''
+
 import numpy as np
 import math
 import logging
@@ -12,60 +22,10 @@ from evaluate import *
 from utils import *
 from gensim.models import KeyedVectors
 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 
 
-class TextDataSet(Dataset):
-
-    def __init__(self, data_path):
-        # Read the dictionary
-        self.word2ids = {word: i for i, word in enumerate(json.load(open(vocab_path, 'r', encoding='utf-8')))}
-        self.id2words = {val: key for key, val in self.word2ids.items()}
-
-        # Read the data
-        self.datas = list(json.load(open(data_path, 'r', encoding='utf-8')).values())
-
-    def __getitem__(self, item):
-        text = self.datas[item]
-        word_to_ids, tag_to_ids = [], []
-        eeg_list, et_list = [], []
-        i, text_len = 0, len(text)
-        attention_mask = []
-        while i < max_length:
-            if i < text_len:
-                word = text[i]
-                word_id = self.word2ids['[UNK]']
-                if word[0] in self.word2ids.keys():
-                    word_id = self.word2ids[word[0]]
-                tag_id = tag2ids[word[-1]]
-                et = list(map(float, word[1: 18]))
-                eeg = list(map(float, word[18: -1]))
-
-                word_to_ids.append(word_id)
-                tag_to_ids.append(tag_id)
-                et_list.append(et)
-                eeg_list.append(eeg)
-                attention_mask.append(1)
-            else:
-                word_to_ids.append(self.word2ids['[PAD]'])
-                tag_to_ids.append(tag2ids['[PAD]'])
-                et_list.append([0.0] * 17)
-                eeg_list.append([0.0] * 8)
-                attention_mask.append(0)
-            i += 1
-        return {
-            "input_ids": torch.tensor(word_to_ids).long().to(device),
-            "tags": torch.tensor(tag_to_ids).long().to(device),
-            "et": torch.tensor(et_list).float().to(device),
-            "eeg": torch.tensor(eeg_list).float().to(device),
-            "attention_mask": torch.tensor(attention_mask).byte().to(device),
-            "text_len": text_len
-        }
-
-    def __len__(self):
-        return len(self.datas)
-
-
+# LayerNorm Function for the self-attention layer
 class LayerNorm(nn.Module):
     # Transform the q,k,v vector with dimension [batch_size * seq_length * hidden_size] into [batch_size * num_attention_heads * seq_length * attention_head_size]，便于后面做 Multi-Head Attention。
     def __init__(self, hidden_size, eps=1e-12):
@@ -83,6 +43,7 @@ class LayerNorm(nn.Module):
         return self.weight * x + self.bias
 
 
+# Define Self-Attention Mechanism Function
 class SelfAttention(nn.Module):
     def __init__(self, num_attention_heads, input_size, hidden_size, hidden_dropout_prob):
         super(SelfAttention, self).__init__()
@@ -153,12 +114,12 @@ class SelfAttention(nn.Module):
         return hidden_states
 
 
-
-class satt_BiLSTM_CRF(nn.Module):
+# Define SATT_BiLSTM_CRF_GL() model
+class SATT_BiLSTM_CRF_GL(nn.Module):
 
     def __init__(self, vocab_size, embed_dim, hidden_dim,
                  num_layers=1, num_tags=6):
-        super(satt_BiLSTM_CRF, self).__init__()
+        super(SATT_BiLSTM_CRF_GL, self).__init__()
 
         # load pretrained GloVe vectors
         pretrain_model = KeyedVectors.load_word2vec_format(word2vecFile, binary=False)
@@ -216,23 +177,23 @@ class satt_BiLSTM_CRF(nn.Module):
 
 def satt_blcrf_glove():
 
-    trainLoader = DataLoader(TextDataSet(train_path), batch_size=batch_size)
-    testLoader = DataLoader(TextDataSet(test_path), batch_size=batch_size)
+    # Load the data
+    trainLoader = DataLoader(dataset=TextDataSet(train_path, vocab_path, max_length, tag2ids), batch_size=batch_size)
+    testLoader = DataLoader(TextDataSet(test_path, vocab_path, max_length, tag2ids), batch_size=batch_size)
 
-    model = satt_BiLSTM_CRF(vocab_size=vocab_size,
+    # Define the model
+    model = SATT_BiLSTM_CRF_GL(vocab_size=vocab_size,
                             embed_dim=embed_dim,
                             hidden_dim=hidden_dim).to(device)
-    # print the model into txt
-    print(model)
-    logging.info(model)
-
+    # Define the optimizer
     optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-    # Train the model
     best_P, best_R, best_F ,best_epoch = 0.0, 0.0, 0.0, 0
     best_epoch, best_loss = 0, 10
     for epoch in range(epochs):
         print("epoch "+ str(epoch + 1) + " is starting!")
+
+        # Train the model
         model.train()
         avg_loss = []
         with tqdm(trainLoader) as pbar_train:
@@ -288,7 +249,4 @@ def satt_blcrf_glove():
         logging.info("R is :" + R_str)
         logging.info("F is :" + F_str)
 
-    logging.info("best epoch is :" + str(best_epoch))
-
     return best_P, best_R, best_F, best_epoch
-
